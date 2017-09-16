39b6b40a01f53af8e73e21c95af7cbaa7f67c445
Shaii Ong <monsteronfire@gmail.com>
2017-09-16 22:13:31 +0800

    Add Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="search">
+    <h1>Nasa Search</h1>
+    <form v-on:submit.prevent="getResult(query)">
+      <input type="text" placeholder="Type in your search" v-model="query" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data () {
+    return {
+      msg: 'Search'
+    }
+  },
+  methods: {
+    getResult(query) {
+      alert(query);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
